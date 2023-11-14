6ea96127d1d6ec09397baf71c5966f87c6271348
ductai202 <ductainguyentran@gmail.com>
2023-11-14 23:42:09 +0700

    add unit test

---
+++ UnitTest/Array n String/Majority_Element_Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using L337C0d3.Array_n_String;
+namespace UnitTest.Array_n_String
+{
+    public class Majority_Element_Test
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 2, 3 }, 3)]
+        [InlineData(new int[] { 2, 2, 1, 1, 1, 2, 2 }, 2)]
+        public void Is_Majority_Element(int[] nums, int value)
+        {
+            var me = new Majority_Element();
+            int result = me.MajorityElement(nums);
+
+            Assert.Equal(value, result);
+        }
+    }
+}
---
+++ UnitTest/Array n String/Merge_Sorted_Array.cs
@@ -0,0 +1,24 @@
+﻿using L337C0d3.Array_n_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Array_n_String
+{
+    public class Merge_Sorted_Array
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 })]
+        [InlineData(new int[] { 1 }, 1, new int[] {}, 0, new int[] {1})]
+        [InlineData(new int[] { 0 }, 0, new int[] {1}, 1, new int[] { 1 })]
+        public void Merge_Sorted_Array_Success(ref int[] nums1, int m, int[] nums2, int n, int[] value)
+        {
+            var msa = new MergeSortedArray();
+            msa.Merge(nums1, m, nums2, n);
+
+            Assert.Equal(value, nums1);
+        }
+    }
+}
