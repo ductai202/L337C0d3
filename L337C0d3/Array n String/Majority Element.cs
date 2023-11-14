6ace5a33fea86c07470299fd8a662eb8a5e2fc64
ductai202 <ductainguyentran@gmail.com>
2023-11-14 08:43:40 +0700

    add new project

---
+++ L337C0d3/Array n String/Majority Element.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L337C0d3.Array_n_String
+{
+    public class Majority_Element
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> myDict = new Dictionary<int, int>();
+            int len = nums.Length / 2;
+            foreach (int i in nums)
+            {
+                if (myDict.ContainsKey(i))
+                {
+                    myDict[i]++;
+                }
+                else
+                {
+                    myDict.Add(i, 1);
+                }
+                if (myDict[i] > len)
+                {
+                    return i;
+                }
+            }
+            return 1;
+        }
+    }
+}
